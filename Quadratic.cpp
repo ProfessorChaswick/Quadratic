d65a70c1646f8970454a2a9beae9abdb3594da79
ceh2624 <ceh2624@gmail.com>
2022-10-06 20:18:24 -0400

    Added an endl to the last cout...

--- Quadratic.cpp
+++ Quadratic.cpp
@@ -7,7 +7,7 @@ int quadraticEquation(int a, int b, int c);
 int main()
 {
     cout << "This is a test: " << pow(5,2) << endl << endl; //testing the pow function
-    cout << "The answer is " << quadraticEquation(1, -12, -28);
+    cout << "The answer is " << quadraticEquation(1, -12, -28) << endl;
     return 0;
 }
 
